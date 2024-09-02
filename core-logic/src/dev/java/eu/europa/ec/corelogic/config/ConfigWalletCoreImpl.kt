96c92d32f3b2270d9103347765a5ad16114d2262
George Fourtounis <gfour@admin.grnet.gr>
2024-09-03 01:14:32 +0300

    Disable SSL checks

--- core-logic/src/dev/java/eu/europa/ec/corelogic/config/ConfigWalletCoreImpl.kt
+++ core-logic/src/dev/java/eu/europa/ec/corelogic/config/ConfigWalletCoreImpl.kt
@@ -27,6 +27,60 @@ import eu.europa.ec.eudi.wallet.transfer.openid4vp.EncryptionMethod
 import eu.europa.ec.eudi.wallet.transfer.openid4vp.PreregisteredVerifier
 import eu.europa.ec.resourceslogic.R
 
+import android.annotation.SuppressLint
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.logging.Logging
+import java.security.SecureRandom
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import javax.security.cert.CertificateException
+
+
+object ProvideKtorHttpClient {
+
+    @SuppressLint("TrustAllX509TrustManager", "CustomX509TrustManager")
+    fun client(): HttpClient {
+        val trustAllCerts = arrayOf<TrustManager>(
+            object : X509TrustManager {
+                @Throws(CertificateException::class)
+                override fun checkClientTrusted(
+                    chain: Array<java.security.cert.X509Certificate>,
+                    authType: String
+                ) {
+                }
+
+                @Throws(CertificateException::class)
+                override fun checkServerTrusted(
+                    chain: Array<java.security.cert.X509Certificate>,
+                    authType: String
+                ) {
+                }
+
+                override fun getAcceptedIssuers(): Array<java.security.cert.X509Certificate> {
+                    return arrayOf()
+                }
+            }
+        )
+
+        return HttpClient(Android) {
+            install(Logging)
+            engine {
+                requestConfig
+                sslManager = { httpsURLConnection ->
+                    httpsURLConnection.sslSocketFactory = SSLContext.getInstance("TLS").apply {
+                        init(null, trustAllCerts, SecureRandom())
+                    }.socketFactory
+                    httpsURLConnection.hostnameVerifier = HostnameVerifier { _, _ -> true }
+                }
+            }
+        }
+    }
+
+}
+
 internal class WalletCoreConfigImpl(
     private val context: Context,
     private val walletCoreLogController: WalletCoreLogController
@@ -91,6 +145,9 @@ internal class WalletCoreConfigImpl(
                             OpenId4VciManager.Config.ProofType.JWT,
                             OpenId4VciManager.Config.ProofType.CWT
                         )
+                    }
+                    .ktorHttpClientFactory {
+                        ProvideKtorHttpClient.client()
                     }
                     .trustedReaderCertificates(R.raw.eudi_pid_issuer_ut)
                     .build()
