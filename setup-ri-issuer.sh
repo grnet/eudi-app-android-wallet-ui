0e0bd30f1e218063a65220beb76fe9029538bac8
George Fourtounis <gfour@admin.grnet.gr>
2025-02-19 16:59:55 +0200

    Support configuration of non-local issuers

--- setup-ri-issuer.sh
+++ setup-ri-issuer.sh
@@ -2,12 +2,18 @@
 
 if [ "$1" == "" ]; then
     echo "Usage:"
-    echo "setup-ri-issuer.sh ISSUER_REPO_PATH"
+    echo "setup-ri-issuer.sh ISSUER_REPO_PATH [ISSUER_HOSTNAME]"
+    echo "ISSUER_REPO_PATH     the local path of the checked out repo of the issuer fork"
+    echo "ISSUER_HOSTNAME      the hostname of the issuer (if empty, the local IP is used)"
     exit
 fi
 
 mkdir -p network-logic/src/main/res/raw/
 cp $1/cert.pem network-logic/src/main/res/raw/cert.pem
 
-IP=$($1/resolve-ip.sh)
-sed -i -e 's/const val VCI_ISSUER_URL = ".*"/const val VCI_ISSUER_URL = "https:\/\/'${IP}':5000"/g' core-logic/src/dev/java/eu/europa/ec/corelogic/config/ConfigWalletCoreImpl.kt
+if [ "$2" == "" ]; then
+    ISSUER_HOST=$($1/resolve-ip.sh)
+else
+    ISSUER_HOST=$2
+fi
+sed -i -e 's/const val VCI_ISSUER_URL = ".*"/const val VCI_ISSUER_URL = "https:\/\/'${ISSUER_HOST}':5000"/g' core-logic/src/dev/java/eu/europa/ec/corelogic/config/ConfigWalletCoreImpl.kt
