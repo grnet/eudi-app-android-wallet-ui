46c2158d3cbd4e7ba74c4e200b94c09a2f6370c8
George Fourtounis <gfour@admin.grnet.gr>
2025-06-23 12:35:00 +0300

    Delete references to non-existent certificates

--- core-logic/src/dev/java/eu/europa/ec/corelogic/config/ConfigWalletCoreImpl.kt
+++ core-logic/src/dev/java/eu/europa/ec/corelogic/config/ConfigWalletCoreImpl.kt
@@ -84,10 +84,6 @@ internal class WalletCoreConfigImpl(
                         R.raw.eudi_pid_issuer_ut,
                         // Updated certificates
                         R.raw.pidissuerca02_ut,
-                        // POTENTIAL playground
-                        R.raw.intermediaire, R.raw.issuer, R.raw.root,
-                        // Czech root (Warsaw)
-                        R.raw.czech_root,
 //                        // https://github.com/eu-digital-identity-wallet/eudi-srv-web-verifier-endpoint-23220-4-kt/issues/256
 //                        // Certificate generated from: https://registry.serviceproviders.eudiw.dev/
 //                        R.raw.grnet
@@ -98,59 +94,6 @@ internal class WalletCoreConfigImpl(
 //                        // https://github.com/eu-digital-identity-wallet/eudi-srv-web-verifier-endpoint-23220-4-kt/issues/256
 //                        // Certificate generated from: https://registry.serviceproviders.eudiw.dev/
 //                        R.raw.grnet
-                        // Utrecht reader certificates
-                        R.raw.animo_reader_ca,
-                        R.raw.bundesdruckerei_reader_ca,
-                        R.raw.clr_labs_reader_ca,
-                        R.raw.credence_id_reader_ca,
-                        R.raw.fast_enterprises_reader_ca,
-                        R.raw.fime_reader_ca_1,
-                        R.raw.fime_reader_ca_2,
-                        R.raw.google_reader_ca,
-                        R.raw.idakto_reader_ca,
-                        R.raw.idemia_reader_ca,
-                        R.raw.in_groupe_reader_ca,
-                        R.raw.lapid_reader_ca,
-                        R.raw.lt_reader_ca,
-                        R.raw.mattr_reader_ca,
-                        R.raw.nearform_reader_ca,
-                        R.raw.ogcio_reader_ca,
-                        R.raw.panasonic_reader_ca,
-                        R.raw.rdw_test_reader_ca,
-                        R.raw.scytales_reader_ca,
-                        R.raw.spruceid_reader_ca,
-                        R.raw.thales_reader_ca_1,
-                        R.raw.thales_reader_ca_2,
-                        R.raw.thales_root_ca,
-                        R.raw.toppan_reader_ca,
-                        R.raw.veridos_reader_ca,
-                        R.raw.zetes_reader_ca,
-                        // Online interop event reader certificates (April '25)
-                        R.raw.a_sit_verifier,
-                        R.raw.a_sit_verifier_ca,
-                        R.raw.amadeus_travelready,
-                        R.raw.animo_root,
-                        R.raw.iaca_ants_fr_idakto_4,
-                        R.raw.ants_fr_idakto_reader_trusted_certificates,
-                        R.raw.idakto_vp,
-                        R.raw.citybee_iaca_lt_potential,
-                        R.raw.verifier_citybee_ds_0001_lt_dev,
-                        R.raw.lapid_ca,
-                        R.raw.mattr_dc_api_verifier,
-                        R.raw.mattr_main_verifier,
-                        R.raw.nf_verifier,
-                        R.raw.panasonic_connect_iaca_cacert,
-                        R.raw.panasonic_connect_reader,
-                        R.raw.toppan_goid_mdl_reader_ca_cer,
-                        R.raw.toppan_hid_global_certificate,
-                        R.raw.toppan_mdl_iaca,
-                        R.raw.zetes_ca,
-                        R.raw.funke_animo_id,
-                        R.raw.lapid_reader,
-                        // POTENTIAL tests
-                        R.raw.lissi1,
-                        R.raw.lissi2,
-                        R.raw.lissi3,
                     )
                 }
             }
