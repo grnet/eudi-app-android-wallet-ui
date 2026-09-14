6c2e9517087ff45a2778f597c19da8ff4eade551
George Fourtounis <gfour@admin.grnet.gr>
2026-09-14 10:04:01 +0300

    Switch to ITB issuer

--- core-logic/src/dev/java/eu/europa/ec/corelogic/config/WalletCoreConfigImpl.kt
+++ core-logic/src/dev/java/eu/europa/ec/corelogic/config/WalletCoreConfigImpl.kt
@@ -130,8 +130,55 @@ internal class WalletCoreConfigImpl : WalletCoreConfig {
 
     override val issuersConfig: List<VciConfig>
         get() = listOf(
+//            VciConfig(
+//                issuerUrl = "https://ec.dev.issuer.eudiw.dev",
+//                config = OpenId4VciManager.Config.Builder()
+//                    .withClientAuthenticationType(
+//                        OpenId4VciManager.ClientAuthenticationType.AttestationBased(
+//                            clientId = "eudiw-abca"
+//                        )
+//                    )
+//                    .withAuthFlowRedirectionURI(BuildConfig.ISSUE_AUTHORIZATION_DEEPLINK)
+//                    .withParUsage(OpenId4VciManager.Config.ParUsage.IF_SUPPORTED)
+//                    .withDPopConfig(DPopConfig.Default)
+//                    .withSupportedCredentialReusePolicies(
+//                        CredentialReusePolicies.Supported(
+//                            policyTypes = setOf(
+//                                EudiReusePolicyType.RotatingBatch,
+//                                EudiReusePolicyType.OnceOnly,
+//                                EudiReusePolicyType.LimitedTime,
+//                            )
+//                        )
+//                    )
+//                    .build(),
+//                order = 0
+//            ),
+//            VciConfig(
+//                issuerUrl = "https://dev.issuer-backend.eudiw.dev",
+//                config = OpenId4VciManager.Config.Builder()
+//                    .withClientAuthenticationType(
+//                        OpenId4VciManager.ClientAuthenticationType.AttestationBased(
+//                            clientId = "eudiw-abca"
+//                        )
+//                    )
+//                    .withAuthFlowRedirectionURI(BuildConfig.ISSUE_AUTHORIZATION_DEEPLINK)
+//                    .withParUsage(OpenId4VciManager.Config.ParUsage.IF_SUPPORTED)
+//                    .withDPopConfig(DPopConfig.Default)
+//                    .withSupportedCredentialReusePolicies(
+//                        CredentialReusePolicies.Supported(
+//                            policyTypes = setOf(
+//                                EudiReusePolicyType.RotatingBatch,
+//                                EudiReusePolicyType.OnceOnly,
+//                                EudiReusePolicyType.LimitedTime,
+//                            )
+//                        )
+//                    )
+//                    .build(),
+//                order = 1
+//            ),
             VciConfig(
-                issuerUrl = "https://ec.dev.issuer.eudiw.dev",
+//                issuerUrl = "https://snf-74864.ok-kno.grnetcloud.net:5602",
+                issuerUrl = "https://dss.aegean.gr/aptitude/",
                 config = OpenId4VciManager.Config.Builder()
                     .withClientAuthenticationType(
                         OpenId4VciManager.ClientAuthenticationType.AttestationBased(
@@ -152,52 +199,6 @@ internal class WalletCoreConfigImpl : WalletCoreConfig {
                     )
                     .build(),
                 order = 0
-            ),
-            VciConfig(
-                issuerUrl = "https://dev.issuer-backend.eudiw.dev",
-                config = OpenId4VciManager.Config.Builder()
-                    .withClientAuthenticationType(
-                        OpenId4VciManager.ClientAuthenticationType.AttestationBased(
-                            clientId = "eudiw-abca"
-                        )
-                    )
-                    .withAuthFlowRedirectionURI(BuildConfig.ISSUE_AUTHORIZATION_DEEPLINK)
-                    .withParUsage(OpenId4VciManager.Config.ParUsage.IF_SUPPORTED)
-                    .withDPopConfig(DPopConfig.Default)
-                    .withSupportedCredentialReusePolicies(
-                        CredentialReusePolicies.Supported(
-                            policyTypes = setOf(
-                                EudiReusePolicyType.RotatingBatch,
-                                EudiReusePolicyType.OnceOnly,
-                                EudiReusePolicyType.LimitedTime,
-                            )
-                        )
-                    )
-                    .build(),
-                order = 1
-            ),
-            VciConfig(
-                issuerUrl = "https://snf-74864.ok-kno.grnetcloud.net:5602",
-                config = OpenId4VciManager.Config.Builder()
-                    .withClientAuthenticationType(
-                        OpenId4VciManager.ClientAuthenticationType.AttestationBased(
-                            clientId = "eudiw-abca"
-                        )
-                    )
-                    .withAuthFlowRedirectionURI(BuildConfig.ISSUE_AUTHORIZATION_DEEPLINK)
-                    .withParUsage(OpenId4VciManager.Config.ParUsage.IF_SUPPORTED)
-                    .withDPopConfig(DPopConfig.Default)
-                    .withSupportedCredentialReusePolicies(
-                        CredentialReusePolicies.Supported(
-                            policyTypes = setOf(
-                                EudiReusePolicyType.RotatingBatch,
-                                EudiReusePolicyType.OnceOnly,
-                                EudiReusePolicyType.LimitedTime,
-                            )
-                        )
-                    )
-                    .build(),
-                order = 2
             )
         )
 
